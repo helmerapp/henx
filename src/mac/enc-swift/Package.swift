47a16f5874164c7af16d808b27b9522d2c3e14f4
clearlysid <siddharth99c@gmail.com>
2024-04-24 17:32:33 +0530

    chore: update url imports and naming structure

--- src/mac/enc-swift/Package.swift
+++ src/mac/enc-swift/Package.swift
@@ -17,14 +17,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "SwiftRs", url: "https://github.com/Brendonovich/swift-rs", from: "1.0.5")
+        .package(url: "https://github.com/Brendonovich/swift-rs", from: "1.0.5")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "enc-swift",
-            dependencies: [.product(name: "SwiftRs", package: "SwiftRs")],
+            dependencies: [.product(name: "SwiftRs", package: "swift-rs")],
             path: "src")
     ]
 )
