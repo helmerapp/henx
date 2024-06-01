ae76555fbb33e9fc0bb68ff7eeb2f301137f1cde
clearlysid <siddharth99c@gmail.com>
2024-06-01 11:34:57 +0530

    chore: remove comments

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,16 @@ use scap::{
 };
 
 fn main() {
-    // #1 Check if the platform is supported
     if !scap::is_supported() {
         println!("❌ Platform not supported");
         return;
     }
 
-    // #2 Check if we have permission to capture the screen
     if !scap::has_permission() {
         println!("❌ Permission not granted");
         return;
     }
 
-    // Scap Options
     let options = Options {
         fps: 60,
         target: None,
@@ -33,31 +30,26 @@ fn main() {
         ..Default::default()
     };
 
-    // Create Recorder
     let mut recorder = Capturer::new(options);
     let [output_width, output_height] = recorder.get_output_frame_size();
 
-    // Create Encoder
     let mut encoder = VideoEncoder::new(VideoEncoderOptions {
         width: output_width as usize,
         height: output_height as usize,
         path: "output.mp4".to_string(),
     });
 
-    // #6 Start Capture
     recorder.start_capture();
 
     // #7 Capture 100 frames
-    for _ in 0..100 {
+    for _ in 0..120 {
         let frame = recorder.get_next_frame().expect("Error");
         let _ = encoder
             .ingest_next_frame(&frame)
             .expect("frame couldn't be encoded");
     }
 
-    // #8 Stop Capture
     recorder.stop_capture();
 
-    // Stop Encoder
-    let _ = encoder.finish();
+    encoder.finish().expect("Failed to finish encoding");
 }
